6b4c106319c491c2e31938e7b7169dd0787f6fba
nowowc1024 <cn150891@student.sgh.waw.pl>
2026-03-14 18:59:20 +0000

    Main Java uopdate

--- src/main/java/com/example/techcorp/Main.java
+++ src/main/java/com/example/techcorp/Main.java
@@ -6,15 +6,28 @@ public class Main {
  
         Employee anna  = new Employee("Anna",  8, 7000);
         Employee piotr = new Employee("Piotr", 6, 6500);
- 
+        Employee marta = new Employee("Marta", 9, 8000);
+
         company.hire(anna);
         company.hire(piotr);
+        company.hire(marta);
  
-        Project project = new Project("Mobile App", 30);
+        Project project = new Project("Mobile App", 100);
         project.addEmployee(anna);
         project.addEmployee(piotr);
+        project.addEmployee(marta);
         company.startProject(project);
+        Project project2 = new Project("Website Redesign", 50);
+        project2.addEmployee(marta);
+        company.startProject(project2);
+
  
+        int turns = 0;
+        while (!project.isComplete()) {
         project.workOneTurn();
+        turns++;
+}
+System.out.println("Completed in " + turns + " turn(s).");
+
     }
 }
